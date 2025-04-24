075839083b9109a05641c990ab73d8d1057964b9
DanelcaN13 <mcgs09930@gmail.com>
2025-04-24 11:42:24 -0500

    Primer commit

--- app/src/main/java/com/example/qta2/MainActivity.kt
+++ app/src/main/java/com/example/qta2/MainActivity.kt
@@ -1,35 +1,25 @@
-package com.example.qta2
+package com.example.qtad
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.example.qta2.databinding.ActivityMainBinding
+import com.example.qta2.logintod
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)  // Vincula este archivo con el layout 'activity_main.xml'
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        // Botón de Iniciar Sesión
+        val iniciarSesionBtn = findViewById<Button>(R.id.login_button)
 
-        val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        // Acción al pulsar el botón
+        iniciarSesionBtn.setOnClickListener {
+            // Navegar hacia la actividad de Login (LoginActivity)
+            val intent = Intent(this, logintod::class.java)
+            startActivity(intent)
+        }
     }
 }
